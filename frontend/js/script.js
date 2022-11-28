1c74cbc643038bbe072f4adf8ba302b0cc74a47d
Alexis Pennel <alexispennel@gmail.com>
2022-11-28 22:54:13 +0100

    gestion des erreurs page d'accueil

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,29 +1,32 @@
-
 const getCanap = async (url) => {
-
   const response = await fetch(url);
-  return await response.json();
+  if (response.ok) {
+    return await response.json();
+  } else {
+    return canapData = -1;
+  }
 };
 
-
 const main = async () => {
-
   const items = document.querySelector("#items");
-  const canapData = await getCanap("http://localhost:3000/api/products");
+  let canapData = await getCanap("http://localhost:3000/api/products");
   let cards = "";
   console.log(canapData);
 
-  canapData.forEach(element => {
-    // productCard(element);
-    cards += `<a href="./product.html?${element._id}">
-        <article>
-          <img src="${element.imageUrl}" alt="${element.altTxt}">
-          <h3 class="productName">${element.name}</h3>
-          <p class="productDescription">${element.description}</p>
-        </article>
-      </a>`
-  })
-  items.innerHTML = cards;
+  if (canapData === -1) {
+    alert("erreur");
+  } else {
+    canapData.forEach(element => {
+      cards += `<a href="./product.html?${element._id}">
+          <article>
+            <img src="${element.imageUrl}" alt="${element.altTxt}">
+            <h3 class="productName">${element.name}</h3>
+            <p class="productDescription">${element.description}</p>
+          </article>
+        </a>`
+    })
+    items.innerHTML = cards;
+  }
 };
 
 main();
